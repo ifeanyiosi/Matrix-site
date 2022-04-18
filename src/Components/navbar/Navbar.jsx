dee5fc224ae71ed7a21f4fc03154300c1bf7dbe7
Ifeanyi Osi-Okeke <ifeanyi.osi.okeke@gmail.com>
2022-04-18 16:43:27 +0100

    update

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
-import './navbar.css'
+import {RiMenu3Line, RiCloseLin} from 'react-icons/ri'
+import './navbar.scss'
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='matrix__navbar'>
+
+      <div className='matrix__navbar-links'>
+
+        <div className='matrix__navbar-links_logo'>
+          <img className='logo-matrix' src={logo} alt="" />
+
+        </div>
+
+        <div className='matrix__navbar-links-container'>
+
+          <p className='links-text' >
+              <a className='links' href="#home">Home</a>
+              <a href="#whatmatrix">What is The Matrix</a>
+              <a href="#possibility">Skynet AI</a>
+              <a href="#features">Case Studies</a>
+              <a href="#blog">Library</a>
+          </p>
+
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
