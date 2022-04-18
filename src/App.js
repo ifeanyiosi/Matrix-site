43cef142f9807299463cc00398bd4d88fcf351f2
Ifeanyi Osi-Okeke <ifeanyi.osi.okeke@gmail.com>
2022-04-18 01:53:29 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react'
 
 
+import {Footer, Header, Possibility, WhatMatrix, Features, Blog} from './Containers'
+import {CTA, Brand, Navbar } from './Components'
+import './App.css'
 
 const App = () => {
   return (
-    <div>
-        <h1>Welcome to the Matrix</h1>
+    <div className='App'>
+
+      <div className='gradient__bg'>
+        <Navbar/>
+        <Header/>
+        
+
+      </div>
+      <Brand/>
+      <WhatMatrix/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
+        
     </div>
   )
 }
--- src/Containers/whatMatrix/WhatMatrix.jsx
+++ src/Containers/whatMatrix/WhatMatrix.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './whatMatrix'
+import './whatMatrix.css'
 
 const WhatMatrix = () => {
   return (
