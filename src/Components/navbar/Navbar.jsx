c1fa227d6a5725d76e43d166f91c62ca84104dac
Ifeanyi Osi-Okeke <ifeanyi.osi.okeke@gmail.com>
2022-04-20 13:31:49 +0100

    ..

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {RiMenu3Line, RiCloseLin} from 'react-icons/ri'
 import './navbar.scss'
 import logo from '../../assets/logo.png'
 
+const Menu = () => (
+  <>
+   <p className='links-text' >
+              <a className='links' href="#home">Home</a></p>
+          <p><a href="#whatmatrix">What is  The Matrix</a></p>
+          <p><a href="#possibility">Skynet AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+  </>
+)
+
 const Navbar = () => {
+     const [toggleMenu, setToggleMenu] = useState(false)
+
+     
+
   return (
     <div className='matrix__navbar'>
 
@@ -16,12 +31,7 @@ const Navbar = () => {
 
         <div className='matrix__navbar-links-container'>
 
-          <p className='links-text' >
-              <a className='links' href="#home">Home</a></p>
-          <p><a href="#whatmatrix">What is  The Matrix</a></p>
-          <p><a href="#possibility">Skynet AI</a></p>
-          <p><a href="#features">Case Studies</a></p>
-          <p><a href="#blog">Library</a></p>
+         <Menu/>
 
            
               
@@ -29,6 +39,33 @@ const Navbar = () => {
               
 
         </div>
+      </div>
+
+      <div className='matrix__navbar-sign'>
+
+        <p className='sign-in'>Sign In</p>
+        <button type='button'>Sign Up</button>
+
+      </div>
+
+      <div className='matrix__navbar-menu'>
+
+          {toggleMenu ? <RiCloseLine color="fff" size={27} onClick={() => setToggleMenu(false)} /> : <RiMenu3Line color="fff" size={27} onClick={() => setToggleMenu(true)} />  
+          }
+          {toggleMenu && (
+            <div className='matrix__navbar-menu-container scale-up-center'>
+                <div className='matrix__navbar-menu-container-links'>
+                  <Menu/>  
+                  <div className='matrix__navbar-menu-container-links-sign'>
+
+        <p className='sign-in'>Sign In</p>
+        <button type='button'>Sign Up</button>
+
+      </div>
+                </div>
+            </div>
+          )}
+
       </div>
       
     </div>
