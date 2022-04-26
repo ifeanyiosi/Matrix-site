afcb1d5e1e977beb846eff9cf14db5968095e0a0
Ifeanyi Osi-Okeke <ifeanyi.osi.okeke@gmail.com>
2022-04-26 13:14:44 +0100

    c

--- src/Components/feature/Feature.jsx
+++ src/Components/feature/Feature.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import './feature.scss'
 
+
+const featuresData = [
+  title:'Improving end distrusts instantly',
+  text: ''
+]
+
 const Feature = () => {
   return (
     <div className='matrix__feature section__padding'>
@@ -8,8 +14,17 @@ const Feature = () => {
       <div className='matrix__feature-heading'>
 
         <h1 className='gradient__text'>
-          
+          The Future is Now and You Just Need To Realize it. Step into the Future Today & Make it Happen
         </h1>
+
+        <p>
+          Request Early Acess to Get Started
+        </p>
+
+      </div>
+
+      <div className='matrix__feature-container'>
+
 
       </div>
       
