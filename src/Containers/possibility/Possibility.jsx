b3ac81c5f3c03d7c6217899d6343f5b8ef6d5564
Ifeanyi Osi-Okeke <ifeanyi.osi.okeke@gmail.com>
2022-04-26 15:16:22 +0100

    ..

--- src/Containers/possibility/Possibility.jsx
+++ src/Containers/possibility/Possibility.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './possibility.css'
 
+import possibility from '../../assets/possibility.jpg'
+
 const Possibility = () => {
   return (
     <div className='matrix__possibility section_padding' id='possibility'>
 
       <div className='matrix__possibility-image'>
-        <img src="" alt="" />
+        <img src={possibility} alt="" />
       </div>
 
     </div>
